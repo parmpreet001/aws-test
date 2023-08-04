3045d07b65a13580e74dc75dc115f30272765565
parmpreet001 <parmpreet1337@gmail.com>
2023-08-03 17:59:44 -0700

    Add /register endpoint to server

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,13 @@ const cors = require('cors')
 const YAML = require('yamljs')
 const swaggerUI = require('swagger-ui-express')
 const swaggerDoc = YAML.load('swagger.yml')
+const sqlite3 = require('sqlite3').verbose();
+const bcrypt = require('bcrypt')
+const bodyParser = require('body-parser');
+
+const sqlFunctions = require('./sqlFunctions.js')
+
+const db = new sqlite3.Database('database.db'); 
 
 const PORT = 5000;
 
@@ -18,10 +25,12 @@ app.use(cors({
   origin: '*',
 }));
 
+app.use(bodyParser.json());
+
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 app.get('/test', (req, res) => {
-	res.json({message: "Node server works!"});
+	res.json("Node server works!");
 	console.log("reached test endpoint")
 });
 
@@ -29,6 +38,30 @@ app.get('/', (req, res) => {
 	res.json({ok: true});
 	console.log("reached root endpoint")
 });
+
+//REGISTRATION AND LOGIN
+app.post("/register", (req, res) => {
+	const {username, password} = req.body;
+
+	bcrypt.hash(password, 10)
+		.then((hash) => {
+	  	return sqlFunctions.insertNewUser(db, username, hash)
+		})
+		.then(() => {
+			console.log("returning with code 200");
+			res.status(200).send();
+		})
+		.catch(() => {
+			console.log("returning with code 500");
+			res.status(500).send();
+		})
+})
+
+app.post("/login", (req, res) => {
+	res.json("login");
+})
+
+
 
 
 const server = https.createServer(options, app);
