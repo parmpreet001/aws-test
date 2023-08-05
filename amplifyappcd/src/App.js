75b58921e7a0ea866112e7a16c7eeb3e284aad29
parmpreet001 <parmpreet1337@gmail.com>
2023-08-04 20:34:28 -0700

    Replace fetch request in App.js with Axios

--- amplifyappcd/src/App.js
+++ amplifyappcd/src/App.js
@@ -1,20 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
+import LoginForm from './Components/LoginForm'
+import axios from 'axios';
 
 function App() {
   const proxy = 'https:localhost:5000'
   const [data, setData] = useState([]);
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch(proxy + '/test');
-      const jsonData = await response.json();
-      setData(jsonData);
-      console.log(`Successfuly retrieved data from node endpoint`)
-    } catch(error) {
-      console.error("Error fetching data:", error);
-    }
+  const fetchData = () => {
+    axios.get(proxy + '/test')
+    .then((response) => {
+      setData(response.data)
+    }).catch((err) => {
+      console.log(err);
+    });
   }
 
   useEffect(() => {
@@ -31,6 +31,9 @@ function App() {
         </p>
         <div>
           {data === null ? <p>Loading...</p> : <p>{data}</p>}
+        </div>
+        <div>
+          <LoginForm/>
         </div>
 
       </header>
