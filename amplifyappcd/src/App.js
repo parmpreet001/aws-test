0dd991c1a4c18cabcb8eb7b39750c5040e3fb7bb
parmpreet001 <parmpreet1337@gmail.com>
2023-08-05 20:40:18 -0700

    Remove EC2 private ip from App.js

--- amplifyappcd/src/App.js
+++ amplifyappcd/src/App.js
@@ -9,8 +9,6 @@ function App() {
 
   if (process.env.NODE_ENV === 'development')
     proxy = 'http://localhost:5000';
-  else
-    proxy = 'https://172.31.13.91:5000'
 
   const [data, setData] = useState([]);
   const [accessToken, setAccessToken] = useState(null);
