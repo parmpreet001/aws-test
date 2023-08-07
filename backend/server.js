7694ac43f0681174251aa631dfaa8b217d71b501
parmpreet001 <parmpreet1337@gmail.com>
2023-08-06 18:50:49 -0700

    Replace sql use in server.js to mongo

--- backend/server.js
+++ backend/server.js
@@ -6,18 +6,33 @@ const cors = require('cors')
 const YAML = require('yamljs')
 const swaggerUI = require('swagger-ui-express')
 const swaggerDoc = YAML.load('swagger.yml')
-const sqlite3 = require('sqlite3').verbose();
+
 const bcrypt = require('bcrypt')
 const cookieParser = require("cookie-parser")
 const JWT = require('./JWT')
 require('dotenv').config();
 
+const mongoose = require('mongoose')
+const User = require('./models/user')
+const Channel = require('./models/channel')
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = "mongodb+srv://master_user:master_user@slack-clone.xt61ykb.mongodb.net/slack-clone-db?retryWrites=true&w=majority";
+
+mongoose.connect(uri)
+	.then((result) => {
+		console.log("Connected to db")
+	})
+	.catch((err) => {
+		console.log(err);
+	})
+
 const bodyParser = require('body-parser');
 
+const { reject } = require('bcrypt/promises');
 
-const sqlFunctions = require('./sqlFunctions.js')
 
-const db = new sqlite3.Database('database.db'); 
 
 const PORT = 5000;
 
@@ -59,40 +74,42 @@ app.post("/register", (req, res) => {
 	const {username, password} = req.body;
 
 	bcrypt.hash(password, 10)
-		.then((hash) => {
-	  	return sqlFunctions.insertNewUser(db, username, hash)
+		.then((hash) => {	
+			const user = new User({username: username, password: hash});
+			return user.save()
 		})
 		.then(() => {
-			res.status(200).send("Successfully registered new user");
+			res.status(200).json("Succesfully added new user")
 		})
-		.catch(() => {
-			console.log("Incorrect username or password");
-			res.status(400).send();
+		.catch((err) => {
+			res.status(400).json(err);
 		})
 })
 
 app.post("/login", (req, res) => {
 	const {username, password} = req.body;
 
-	sqlFunctions.userExists(db, username)
-		.then(() => {
-			return sqlFunctions.getHashedPassword(db, username);
+	User.find({username: username})
+		.then((result) => {
+			if (result === undefined || result.length === 0)
+				throw("Invalid Username")
+
+			const user = result[0];
+			return user.password;
 		})
 		.then((hashedPassword) => {
-			return bcrypt.compare(password, hashedPassword)
+			return bcrypt.compare(password, hashedPassword);
 		})
 		.then((match) => {
 			if (match) {
-				const accessToken = JWT.createToken({username: username});
-				console.log(accessToken);
+				const accessToken = JWT.createToken({username: username})
 				res.status(200).json({accessToken: accessToken})
-			}	
-			else {
-				res.status(400).json("Invalid Password");
 			}
+			else
+				res.status(400).json("Invalid Password")
 		})
 		.catch((err) => {
-			res.status(400).json(err);
+			res.status(400).json(err)
 		})
 })
 
@@ -107,24 +124,24 @@ app.post("/profile", JWT.validateToken, (req, res) => {
 app.post("/addChannel", JWT.validateToken, (req, res) => {
 	const { username, channelName } = req.body;
 
-	sqlFunctions.addChannel(db, username, channelName)
+	const channel = new Channel({channelName: channelName, owner: username})
+
+	channel.save()
 		.then(() => {
 			res.status(200).json("Added Channel");
 		})
 		.catch((err) => {
 			res.status(400).json(err);
 		})
-	// console.log(`Called /addChannel with: ${username} and ${channelName}`)
 })
 
 app.post("/getChannels", JWT.validateToken, (req, res) => {
-	sqlFunctions.getAllChannels(db)
-		.then((rows) => {
-			console.log(rows);
-			res.status(200).json(rows)
+	Channel.find()
+		.then((result) => {
+			res.status(200).json(result);
 		})
 		.catch((err) => {
-			res.status(400).json(err)
+			res.status(400).json(err);
 		})
 })
 
