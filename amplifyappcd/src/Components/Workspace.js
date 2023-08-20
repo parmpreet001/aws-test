3158e9ab7c90a75867eb0c96e580eb1b8934154c
parmpreet001 <parmpreet1337@gmail.com>
2023-08-19 18:22:36 -0700

    Create Workspace.js

---
+++ amplifyappcd/src/Components/Workspace.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect, useContext } from 'react';
+import axios from 'axios';
+
+function Workspace() {
+	return (
+		<div>
+			<div>
+				Item 1
+			</div>
+			<div>
+				Item 2
+			</div>
+		</div>
+	)
+}
+
+
+
+export default Workspace;
