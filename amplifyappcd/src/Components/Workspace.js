917b563bc15fb9c53630b2bd198448a8c3ba3a6d
parmpreet001 <parmpreet1337@gmail.com>
2023-08-21 14:00:25 -0700

    Update Workspace and associated CSS
    - Implemented non functioning navbar
    - Implement responsive webpage between desktop/mobile view

--- amplifyappcd/src/Components/Workspace.js
+++ amplifyappcd/src/Components/Workspace.js
@@ -1,16 +1,63 @@
 import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
+import './Workspace.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft, faHashtag, faUser } from '@fortawesome/free-solid-svg-icons'
+
 
 function Workspace() {
+
+
+	const [isDesktop, setDesktop] = useState(window.innerWidth >= 1024);
+
+	const updateMedia = () => {
+		setDesktop(window.innerWidth >= 1024);
+	}
+
+	useEffect(() => {
+		window.addEventListener("resize", updateMedia);
+		return () => window.removeEventListener("resize", updateMedia);
+	})
+
+	const renderChannels = () => (
+		<div className='channel-container'>
+			{[...Array(40)].map(() => (<div className='channel'>Channel</div>))}
+		</div >
+	)
+
+	const renderUsers = () => (
+		<div className='users-container'>
+		Item 3
+	</div>
+	)
+
+	// NAV BAR
+	const renderNavBarMobile = () => (
+		<nav className='navbar'>
+			<div> <FontAwesomeIcon icon={faArrowLeft}/> </div>
+			<div> <FontAwesomeIcon icon={faHashtag}/> </div>
+			<div> <FontAwesomeIcon icon={faUser}/> </div>
+		</nav>
+	)
+
+	const renderNavBarDesktop = () => (
+		<nav className='navbar'>
+			<div> <FontAwesomeIcon icon={faArrowLeft}/> </div>
+		</nav>
+	)
+
 	return (
 		<div>
-			<div>
-				Item 1
-			</div>
-			<div>
-				Item 2
-			</div>
+			{isDesktop ? renderNavBarDesktop() : renderNavBarMobile()}
+			<div className='workspace-container'>
+				{isDesktop && renderChannels()}
+				<div className='main-container'>
+					{isDesktop ? (<div>Desktop</div>) : (<div>Mobile</div>)}
+				</div>
+				{isDesktop && renderUsers()}
+			</div>			
 		</div>
+
 	)
 }
 
